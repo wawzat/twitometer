2c73dd77a3b598d01e37a67e2fe8c2bd0c04eb82
wawzat <wawzat@gmail.com>
2020-06-14 16:11:01 -0700

    Change stepper value to pos_tweet_rate

--- twitometer.py
+++ twitometer.py
@@ -100,7 +100,8 @@ class MyStreamListener(tweepy.StreamListener):
         self.dict_num_tweets = { i : 0 for i in self.tags}
         self.dict_tweet_rate = { i : 0 for i in self.tags}
         self.dict_sentiment = { i : 0 for i in self.tags}
-        self.dict_sentiment_rate = { i : 0 for i in self.tags}
+        self.dict_pos_tweets = { i : 0 for i in self.tags}
+        self.dict_pos_tweet_rate = { i : 0 for i in self.tags}
         self.current_position = 0
 
     def on_status(self, status):
@@ -141,26 +142,28 @@ class MyStreamListener(tweepy.StreamListener):
                             self.dict_sentiment[tag] -= 1
                             tweet_score -= 1
                             break
-                    #with open(csv_output_file, 'a', encoding='utf_8_sig', newline='') as f_output:
-                        #csv_output = csv.writer(f_output)
-                        #row.append(tag)
-                        #if tweet_score > 0:
-                            #word = pos_word
-                        #elif tweet_score < 0:
-                            #word = neg_word
-                        #else:
-                            #word = " "
-                        #row.append(word)
-                        #row.append(tweet_score)
-                        #row.append(status.author.screen_name)
-                        #row.append(status.source)
-                        #row.append(tweet)
-                        #csv_output.writerow(row)
-                        #row = []
-                        #tweet_score = 0
+                    if self.dict_sentiment[tag] < 0:
+                        self.dict_pos_tweets = self.dict_num_tweets - self.dict_sentiment
+                    #csv_output = csv.writer(f_output)
+                    #row.append(tag)
+                    #if tweet_score > 0:
+                        #word = pos_word
+                    #elif tweet_score < 0:
+                        #word = neg_word
+                    #else:
+                        #word = " "
+                    #row.append(word)
+                    #row.append(tweet_score)
+                    #row.append(status.author.screen_name)
+                    #row.append(status.source)
+                    #row.append(tweet)
+                    #csv_output.writerow(row)
+                    #row = []
+                    #tweet_score = 0
                 self.dict_tweet_rate[tag] = round(self.dict_num_tweets[tag] / elapsed_time.seconds * 60)
-                self.dict_sentiment_rate[tag] = int(self.dict_sentiment[tag] / elapsed_time.seconds * 60)
-                #self.current_position = move_stepper(self.dict_sentiment_rate[tag], self.current_position)
+                self.dict_pos_tweet_rate[tag] = int(self.dict_pos_tweets[tag] / elapsed_time.seconds * 60)
+                if tag == "trump"
+                self.current_position = move_stepper(self.dict_pos_tweet_rate[tag], self.current_position)
             for tag in self.tags:
                 if self.dict_num_tweets[tag] != 0:
                     sentiment_pct = round(self.dict_sentiment[tag] / self.dict_num_tweets[tag], 2)
