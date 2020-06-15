122bf15d996c72d4baa0ed8304ac425d7e600e30
wawzat <wawzat@gmail.com>
2020-06-15 16:56:06 -0700

    Fix error with self.Motors DCMotor not defined. Removed.

--- Raspi_X27_Stepper.py
+++ Raspi_X27_Stepper.py
@@ -98,7 +98,6 @@ class Raspi_MotorHAT:
     def __init__(self, addr = 0x60, freq = 1600):
         self._i2caddr = addr            # default addr on HAT
         self._frequency = freq      # default @1600Hz PWM freq
-        self.motors = [ Raspi_DCMotor(self, m) for m in range(4) ]
         self.steppers = [ Raspi_StepperMotor(self, 1), Raspi_StepperMotor(self, 2) ]
 
     def setPin(self, pin, value):
