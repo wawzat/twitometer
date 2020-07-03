1d4346d98f15a1994bcdac57cfc68255decd6202
wawzat <wawzat@gmail.com>
2020-07-03 12:55:38 -0700

    Make class indicator position variables global not self

--- twitometer.py
+++ twitometer.py
@@ -122,8 +122,8 @@ class MyStreamListener(tweepy.StreamListener):
         self.dict_pos_tweet_rate = { i : 0 for i in self.tags}
         self.current_position_1 = 0
         self.current_position_2 = 0
-        self.indicator_pos_1
-        self.indicator_pos_2
+        global indicator_pos_1
+        global indicator_pos_2
 
 
     def on_status(self, status):
