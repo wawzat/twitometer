15bb9909320e9cdb5c9ae76bd8453a9809dd73b8
wawzat <wawzat@gmail.com>
2020-07-03 12:30:25 -0700

    Move gllobal position vars to on_status()

--- twitometer.py
+++ twitometer.py
@@ -122,10 +122,11 @@ class MyStreamListener(tweepy.StreamListener):
         self.dict_pos_tweet_rate = { i : 0 for i in self.tags}
         self.current_position_1 = 0
         self.current_position_2 = 0
-        global indicator_pos_1
-        global indicator_pos_2
+
 
     def on_status(self, status):
+        global indicator_pos_1
+        global indicator_pos_2
         try:
             #print(status.text)
             #csv_output_file = r"D:\Users\James\OneDrive\Documents\Raspberry Pi-Matrix5\JSL Python Code\Twitter\tweets.csv"
