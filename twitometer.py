be134d8aedbef513acc17ae5a643fb9b2fe6cf25
wawzat <wawzat@gmail.com>
2020-07-23 14:00:52 -0700

    Independently track led write times

--- twitometer.py
+++ twitometer.py
@@ -192,7 +192,8 @@ class MyStreamListener(tweepy.StreamListener):
         self.dict_tpm_pos_tweets = { i : 0 for i in self.tags}
         self.dict_pos_tweet_rate = { i : 0 for i in self.tags}
         self.stepper_write_time = datetime.datetime.now()
-        self.led_write_time = datetime.datetime.now()
+        self.led_write_time_1 = datetime.datetime.now()
+        self.led_write_time_2 = datetime.datetime.now()
         self.indicator_pos_1 = 0
         self.indicator_pos_2 = 0
         self.indicator_pos_1_list = []
@@ -281,18 +282,18 @@ class MyStreamListener(tweepy.StreamListener):
                             if len(self.indicator_pos_1_list) >= 40:
                                 self.indicator_pos_1_list.pop(0)
                             self.indicator_pos_1_list.append(self.indicator_pos_1)
-                            led_elapsed_time = datetime.datetime.now() - self.led_write_time
-                            if led_elapsed_time.seconds >= 40:
-                                self.led_write_time = write_matrix(tweet_1, "1", self.led_write_time)
+                            led_elapsed_time_1 = datetime.datetime.now() - self.led_write_time_1
+                            if led_elapsed_time_1.seconds >= 40:
+                                self.led_write_time_1 = write_matrix(tweet_1, "1", self.led_write_time_1)
                         elif tag == "trump":
                             tweet_2 = tweet
                             self.indicator_pos_2 = min(int(4 * self.dict_tpm[tag] + 150), 3240)
                             if len(self.indicator_pos_2_list) >= 40:
                                 self.indicator_pos_2_list.pop(0)
                             self.indicator_pos_2_list.append(self.indicator_pos_2)
-                            led_elapsed_time = datetime.datetime.now() - self.led_write_time
-                            if led_elapsed_time.seconds >= 40:
-                                self.led_write_time = write_matrix(tweet_2, "2", self.led_write_time)
+                            led_elapsed_time_2 = datetime.datetime.now() - self.led_write_time_2
+                            if led_elapsed_time_2.seconds >= 40:
+                                self.led_write_time_2 = write_matrix(tweet_2, "2", self.led_write_time_2)
                 position1 = statistics.mean(self.indicator_pos_1_list)
                 position2 = statistics.mean(self.indicator_pos_2_list)
                 self.stepper_write_time = move_stepper(str(position1), str(position2), self.stepper_write_time)
